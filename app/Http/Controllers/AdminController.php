d5a64a22f9ed80b453cb570fb862673e58a26a49
anggarivian <anggarivian.11@gmail.com>
2023-01-19 22:45:50 +0700

    Checkout Add

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -9,6 +9,7 @@ use Illuminate\Validation\ValidationException;
 use App\Models\User;
 use App\Models\Kamar;
 use App\Models\Reservasi;
+use App\Models\Checkout;
 use PDF;
 
 class AdminController extends Controller
@@ -241,13 +242,13 @@ class AdminController extends Controller
 
         $kamar = Kamar::all();
         $user = User::all();
-
         $users = User::all();
+        $checkout = Checkout::all();
 
         $user = Auth::user();
         $reservasi = Reservasi::all();
 
-        return view('reservasi',compact('users','kamar','reservasi'));
+        return view('reservasi',compact('users','kamar','reservasi', 'checkout'));
 
     }
 
@@ -320,29 +321,47 @@ class AdminController extends Controller
     }
 
     public function update_reservasi(Request $req) { 
-        $reservasi = Reservasi::find($req->get('id'));
         
         $validate = $req->validate([
-            'picture' => 'required', 
+            'users_id' => 'required',
+            'kamars_id' => 'required',
+            'jumlahkamar' => 'required',
+            'jumlahorang' => 'required',
+            'datein' => 'required',
+            'dateout' => 'required',
         ]);
+
+        $checkout = new Checkout;
         
+        $checkout->users_id = $req->get('users_id');
+        $checkout->kamars_id = $req->get('kamars_id');
+        $checkout->jumlahkamar = $req->get('jumlahkamar');
+        $checkout->jumlahorang = $req->get('jumlahorang');
+        $checkout->datein = $req->get('datein');
+        $checkout->dateout = $req->get('dateout');
+
         if ($req->hasFile('picture')) {
             $extension = $req->file('picture')->extension(); 
-            $filename = 'picture_reservasi_'.time().'.'.$extension;
-            $req->file('picture')->storeAs('public/picture_reservasi', $filename ); 
+            $filename = 'picture_checkout_'.time().'.'.$extension;
+            $req->file('picture')->storeAs('public/picture_checkout', $filename ); 
             
-            Storage::delete('public/picture_reservasi/'.$req->get('old_picture')); 
-            $reservasi->picture = $filename; 
+            Storage::delete('public/picture_checkout/'.$req->get('old_picture')); 
+            $checkout->picture = $filename; 
         } 
-        
-        $reservasi->save(); 
 
-        $notification = array( 
-            'message' => 'Data reservasi berhasil diubah', 
+        $checkout->save();
+
+        $reservasi = Reservasi::find($req->get('id'));
+
+        $reservasi->delete();
+
+        $notification = array(
+            'message' => 'Data checkout berhasil ditambahkan',
             'alert-type' => 'success'
         );
 
-        return redirect()->route('admin.user')->with($notification);
+        return redirect()->route('admin.reservasi')->with($notification);
+
     }
 
     public function delete_Reservasi($id)
@@ -353,6 +372,21 @@ class AdminController extends Controller
 
         $success = true;
         $message = "Data Reservasi berhasil dihapus";
+
+        return response()->json([
+            'success' => $success,
+            'message' => $message,
+        ]);
+    }
+
+    public function delete_Reservasi2($id)
+    {
+        $checkout = Checkout::find($id);
+
+        $checkout->delete();
+
+        $success = true;
+        $message = "Data checkout berhasil dihapus";
 
         return response()->json([
             'success' => $success,
---
+++ database/migrations/2023_01_19_083521_create_checkouts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCheckoutsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('checkouts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('users_id')->constrained();
+            $table->foreignId('kamars_id')->constrained();
+            $table->integer('jumlahkamar');
+            $table->integer('jumlahorang');
+            $table->date('datein');
+            $table->date('dateout');
+            $table->string('picture')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('checkouts');
+    }
+}
